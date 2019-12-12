6b564cdaf30d93bd0d04ae3053a716698abbd004
Nghi Tran <nghi.t.tran8@gmail.com>
2019-12-12 09:45:00 -0800

    drill 4

--- src/api.js
+++ src/api.js
@@ -4,6 +4,16 @@ function getItems() {
   return fetch(`${BASE_URL}/items`);
 }
 
+function createItems(name) {
+  // Properties of the new item
+  let newItem = {name: name};
+  // convert javascript object to json object
+  JSON.stringify(newItem);
+  // fetch the api to create a new item
+  fetch(`${BASE_URL}/items`, {method: 'POST', headers: {'Content-Type': 'application/json'}, body: newItem});
+}
+
 export default {
-  getItems
+  getItems,
+  createItems
 };
